e95656d037456c55bf6860c4659a9531f0985cc2
Aung Hein Oo <heinooa34@gmail.com>
2023-05-21 12:13:31 +0630

    project transfer

--- src/com/ai/model/Category.java
+++ src/com/ai/model/Category.java
@@ -1,5 +1,47 @@
 package com.ai.model;
 
-public class Category {
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Table(name="category")
+
+public class Category implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name="name")
+    private String name;
+	
+	@OneToMany(mappedBy = "category",fetch=FetchType.LAZY,
+    		cascade = CascadeType.ALL)
+    private Set<Category> category;
+
+
+    public Category(){}
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
